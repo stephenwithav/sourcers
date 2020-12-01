b128990d79fca231d4237c69511e3683a49110fc
Steven Edwards <steven@stephenwithav.com>
2020-12-01 10:42:38 -0500

    Add YAML Sourcer.
    This cheats a bit by providing a conf wrapper around go-yaml, similar
    to viper's behavior.
    
    It works for adding simple YAML support to conf, though.
    
    Closes #2.

--- sourcers.go
+++ sourcers.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/ardanlabs/conf"
+	"gopkg.in/yaml.v3"
 )
 
 type JSON struct {
@@ -47,6 +48,56 @@ func NewSourceJSON(r io.Reader) (*JSON, error) {
 // Source implements the conf.Sourcer interface. It returns the
 // stringified value stored at the specified key in the JSON>
 func (j *JSON) Source(fld conf.Field) (string, bool) {
+	if fld.Options.ShortFlagChar != 0 {
+		flagKey := fld.Options.ShortFlagChar
+		k := strings.ToLower(string(flagKey))
+		if val, found := j.m[k]; found {
+			return val, found
+		}
+	}
+
+	k := strings.ToLower(strings.Join(fld.FlagKey, `_`))
+	val, found := j.m[k]
+	return val, found
+}
+
+type YAML struct {
+	m map[string]string
+}
+
+// NewSourceYAML accepts a reader containing a valid YAML struct and
+// returns a ardanlabs/conf.Sourcer for use by that package's Parse
+// method.
+func NewSourceYAML(r io.Reader) (*YAML, error) {
+	src, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	tmpMap := make(map[string]interface{})
+	err = yaml.Unmarshal(src, &tmpMap)
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]string)
+	for key, value := range tmpMap {
+		switch v := value.(type) {
+		case float64:
+			m[key] = strings.TrimRight(fmt.Sprintf("%f", v), "0.")
+		case bool:
+			m[key] = fmt.Sprintf("%t", v)
+		case string:
+			m[key] = value.(string)
+		}
+	}
+
+	return &YAML{m: m}, nil
+}
+
+// Source implements the conf.Sourcer interface. It returns the
+// stringified value stored at the specified key in the YAML>
+func (j *YAML) Source(fld conf.Field) (string, bool) {
 	if fld.Options.ShortFlagChar != 0 {
 		flagKey := fld.Options.ShortFlagChar
 		k := strings.ToLower(string(flagKey))
