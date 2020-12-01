56cd0f857e5b55104770b0af177f4c31edb8b2d9
Steven Edwards <steven@stephenwithav.com>
2020-12-01 13:20:20 -0500

    Refactor Sourcers.
    Each Sourcer repeated the same code block, save for the unique
    Unmarshal call, so this refactoring acknowledges that fact by
    massively simplifying the code.
    
    Now, creating a new Sourcer is simply a matter of adding Unmarshal
    funcs.

--- sourcers.go
+++ sourcers.go
@@ -12,152 +12,71 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type JSON struct {
+type genericSourcer struct {
 	m map[string]string
+}
+
+func (s *genericSourcer) Source(fld conf.Field) (string, bool) {
+	if fld.Options.ShortFlagChar != 0 {
+		flagKey := fld.Options.ShortFlagChar
+		k := strings.ToLower(string(flagKey))
+		if val, found := s.m[k]; found {
+			return val, found
+		}
+	}
+
+	k := strings.ToLower(strings.Join(fld.FlagKey, `_`))
+	val, found := s.m[k]
+	return val, found
+}
+
+type unmarshalFunc func([]byte, interface{}) error
+
+// sourceFrom ...
+func sourceFrom(f unmarshalFunc, r io.Reader) (conf.Sourcer, error) {
+	src, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	tmpMap := make(map[string]interface{})
+	err = f(src, &tmpMap)
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string]string)
+	for key, value := range tmpMap {
+		switch v := value.(type) {
+		case float64:
+			m[key] = strings.TrimRight(fmt.Sprintf("%f", v), "0.")
+		case bool:
+			m[key] = fmt.Sprintf("%t", v)
+		case string:
+			m[key] = value.(string)
+		}
+	}
+
+	return &genericSourcer{m: m}, nil
 }
 
 // NewSourceJSON accepts a reader containing a valid JSON struct and
 // returns a ardanlabs/conf.Sourcer for use by that package's Parse
 // method.
-func NewSourceJSON(r io.Reader) (*JSON, error) {
-	src, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	tmpMap := make(map[string]interface{})
-	err = json.Unmarshal(src, &tmpMap)
-	if err != nil {
-		return nil, err
-	}
-
-	m := make(map[string]string)
-	for key, value := range tmpMap {
-		switch v := value.(type) {
-		case float64:
-			m[key] = strings.TrimRight(fmt.Sprintf("%f", v), "0.")
-		case bool:
-			m[key] = fmt.Sprintf("%t", v)
-		case string:
-			m[key] = value.(string)
-		}
-	}
-
-	return &JSON{m: m}, nil
-}
-
-// Source implements the conf.Sourcer interface. It returns the
-// stringified value stored at the specified key in the JSON>
-func (j *JSON) Source(fld conf.Field) (string, bool) {
-	if fld.Options.ShortFlagChar != 0 {
-		flagKey := fld.Options.ShortFlagChar
-		k := strings.ToLower(string(flagKey))
-		if val, found := j.m[k]; found {
-			return val, found
-		}
-	}
-
-	k := strings.ToLower(strings.Join(fld.FlagKey, `_`))
-	val, found := j.m[k]
-	return val, found
-}
-
-type YAML struct {
-	m map[string]string
+func NewSourceJSON(r io.Reader) (conf.Sourcer, error) {
+	return sourceFrom(json.Unmarshal, r)
 }
 
 // NewSourceYAML accepts a reader containing a valid YAML struct and
 // returns a ardanlabs/conf.Sourcer for use by that package's Parse
 // method.
-func NewSourceYAML(r io.Reader) (*YAML, error) {
-	src, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	tmpMap := make(map[string]interface{})
-	err = yaml.Unmarshal(src, &tmpMap)
-	if err != nil {
-		return nil, err
-	}
-
-	m := make(map[string]string)
-	for key, value := range tmpMap {
-		switch v := value.(type) {
-		case float64:
-			m[key] = strings.TrimRight(fmt.Sprintf("%f", v), "0.")
-		case bool:
-			m[key] = fmt.Sprintf("%t", v)
-		case string:
-			m[key] = value.(string)
-		}
-	}
-
-	return &YAML{m: m}, nil
-}
-
-// Source implements the conf.Sourcer interface. It returns the
-// stringified value stored at the specified key in the YAML>
-func (j *YAML) Source(fld conf.Field) (string, bool) {
-	if fld.Options.ShortFlagChar != 0 {
-		flagKey := fld.Options.ShortFlagChar
-		k := strings.ToLower(string(flagKey))
-		if val, found := j.m[k]; found {
-			return val, found
-		}
-	}
-
-	k := strings.ToLower(strings.Join(fld.FlagKey, `_`))
-	val, found := j.m[k]
-	return val, found
-}
-
-type TOML struct {
-	m map[string]string
+func NewSourceYAML(r io.Reader) (conf.Sourcer, error) {
+	return sourceFrom(yaml.Unmarshal, r)
 }
 
 // NewSourceTOML accepts a reader containing a valid TOML struct and
 // returns a ardanlabs/conf.Sourcer for use by that package's Parse
 // method.
-func NewSourceTOML(r io.Reader) (*TOML, error) {
-	src, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
-
-	tmpMap := make(map[string]interface{})
-	err = toml.Unmarshal(src, &tmpMap)
-	if err != nil {
-		return nil, err
-	}
-
-	m := make(map[string]string)
-	for key, value := range tmpMap {
-		switch v := value.(type) {
-		case float64:
-			m[key] = strings.TrimRight(fmt.Sprintf("%f", v), "0.")
-		case bool:
-			m[key] = fmt.Sprintf("%t", v)
-		case string:
-			m[key] = value.(string)
-		}
-	}
-
-	return &TOML{m: m}, nil
-}
-
-// Source implements the conf.Sourcer interface. It returns the
-// stringified value stored at the specified key in the TOML>
-func (j *TOML) Source(fld conf.Field) (string, bool) {
-	if fld.Options.ShortFlagChar != 0 {
-		flagKey := fld.Options.ShortFlagChar
-		k := strings.ToLower(string(flagKey))
-		if val, found := j.m[k]; found {
-			return val, found
-		}
-	}
-
-	k := strings.ToLower(strings.Join(fld.FlagKey, `_`))
-	val, found := j.m[k]
-	return val, found
+func NewSourceTOML(r io.Reader) (conf.Sourcer, error) {
+	return sourceFrom(toml.Unmarshal, r)
 }
